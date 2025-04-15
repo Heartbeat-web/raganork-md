6aae562ddd523a7829334e9c5cf0cbf9abe9d4e8
Heartbeat-web <xuppyguy@gmail.com>
2025-04-16 00:32:27 +0100

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -96,7 +96,7 @@ Module({on: 'text', fromMe: false}, (async (m, mat) => {
     }
   } 
   if (ANTIWORD_WARN?.split(",").includes(m.jid)){
-    let disallowedWords = (process.env.ANTI_WORDS || "nigga,fuck").split(",");
+    let disallowedWords = (process.env.ANTI_WORDS || "nigga,die,kill,murder,rape,sex,toto,fuck").split(",");
     if (!process.env.ANTI_WORDS || process.env.ANTI_WORDS == 'auto') disallowedWords = require('badwords/array');
     let thatWord = containsDisallowedWords(m.message,disallowedWords)
     if (thatWord){
